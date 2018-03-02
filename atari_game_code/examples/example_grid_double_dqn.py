e52170968b74398f400578185dd2c20b094c0629
Jin Zhu <jinz1@andrew.cmu.edu>
2018-03-02 17:18:04 -0500

    build graph

--- atari_game_code/examples/example_grid_double_dqn.py
+++ atari_game_code/examples/example_grid_double_dqn.py
@@ -5,18 +5,10 @@ import sys
 repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(repo_path)
 sys.path.insert(0, os.path.join(repo_path, 'agents'))
+sys.path.insert(0, os.path.join(repo_path, 'environment'))
 
-from gridworld import gameEnv
+from gridworld import *
 from duel_DQN import *
-import gym
-import numpy as np
-import random
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
-import matplotlib.pyplot as plt
-import scipy.misc
-import os
-
 
 
 env = gameEnv(partial=False,size=5)
